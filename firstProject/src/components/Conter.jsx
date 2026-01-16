551e65899546320a3220d00ec7f427072ccdaab2
Devendra Solanki <devendrasolanki1485@gmail.com>
2026-01-16 10:55:49 +0530

    feat: Remove render method and commented-out code in Conter component for cleaner implementation

--- firstProject/src/components/Conter.jsx
+++ firstProject/src/components/Conter.jsx
@@ -18,15 +18,31 @@ class Conter extends Component {
     );
     // console.log(this.state.count); // Note: This will log the old count value due to the asynchronous nature of setState
   };
+  //here we are trying to increase the count by 5 on a single button click
+//   inscrecmentFive = () => {
+//     this.increaseCount();
+//     this.increaseCount();
+//     this.increaseCount();
+//     this.increaseCount();
+//     this.increaseCount();  
+//   };
 
-  render() {
-    return (
-      <>
-        <h1>Count: {this.state.count}</h1>
-        <button onClick={this.increaseCount}>Increase Count</button>
-      </>
-    );
-  }
+
+
+//   render() {
+//     return (
+//       <>
+//         <h1>Count: {this.state.count}</h1>
+//         <button onClick={this.increaseCount}>Increase Count</button>
+//         <button onClick={this.inscrecmentFive}>Increase Count Five</button>// button to increase count by 5
+//       </>
+//     );
+//   }
+
+
+
+
+
 }
 
 export default Conter;
