4434cc333ea179b8bb1288a5ff95699a7a43f8a0
Devendra Solanki <devendrasolanki1485@gmail.com>
2026-01-22 09:41:37 +0530

    fix: Correct import statement for Counter1 and clean up commented code in App component

--- firstProject/src/App.jsx
+++ firstProject/src/App.jsx
@@ -1,9 +1,8 @@
-import React, { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
 import "./App.css";
+
 import Massage1 from "./components/Massage1";
-import Conter1 from "./components/Counter1";
+import Counter1 from "./components/Counter1";
 import Counter2 from "./components/Conter2";
 import Toggle from "./components/Toggle";
 import InputExample from "./components/InputExample";
@@ -13,95 +12,27 @@ import Fruits from "./components/Fruits";
 import ArrayState from "./components/ArrayState";
 import EmployeeState from "./components/EmployeeState";
 import UserGreeting from "./components/UserGreeting";
-// import Home from "./components/Home";
-// import About from "./components/About";
-// import Hello from "./components/Heder";
-// import Footer from "./components/Footer";
-// import Employee from "./components/Employee";
-// import Student from "./components/Student";
-// import Child from "./components/Child";
-// import Perent from "./components/Perent";
-// import Massages from "./components/Massages";
-// import Conter from "./components/Conter";
 
 function App() {
-  <div>
-    {/* // //01 //return <h1>hello</h1>; // it only returns one element if you want to return multiple elements you have to wrap them in a div or a fragment
-
-  // // 02 return ( // wrapping in a div
-  // //     <div>
-  // //       <h1>hello world</h1>
-
-  // //     </div>
-  // //   );
-
-  // 3  return ( // Fragment syntax
-  //   <>
-  //     <h1>Hello World</h1>
-  //   </>
-  // )
-  // // return (
-  // //   //  React.createElement(h1, null, "Hello World"); // using React.createElement method but this is not common in React
-
-  // //   React.createElement(h1, null, "Hello World")
-  // // ); // using React.createElement method but this is not common in React
-
-  // return ( // 03
-  //   <>
-  //     <h1>welcome</h1>
-  //     <form action="" className="form">
-  //       <label htmlFor="name">Name:</label>
-  //       <input type="text" id="name" name="name" placeholder="enter name" />
-  //       <br />
-  //       <label htmlFor="email">Email:</label>
-  //       <input type="email" id="email" name="email" placeholder="enter Email" />
-  //       <br />
-  //       <label htmlFor="password">Password</label>
-  //       <input */}
-    {/* //         type="password"
-  //         id="password"
-  //         name="password"
-  //         placeholder="enter password"
-  //       />
-  //       <br />
-  //       <input type="submit" value="Submit" />
-  //     </form>
-  //   </>
-  // ); */}
-  </div>;
-
   return (
     <>
-      {/* <Home />;
-      <About />;
-      <Hello />;<Footer />; */}
+      {/* Uncomment only what you want to render */}
 
-      {/* <Employee name="John Doe" lastname="Smith" role="Developer" />
-      <Employee lastname="Smith" role="Developer" />
-      <Student name="Jane Doe" lastname="Johnson" grade="A" />
-      <Student grade="A" />
-      <Employee name="DEV" lastname="Solanki" role="Developer" /> */}
-      {/* <Perent />
-      <Child /> */}
-      {/* <Massages /> */}
-      {/* <Conter /> */}
       {/* <Massage1 /> */}
-
-      {/* <Conter1/> */}
+      {/* <Counter1 /> */}
       {/* <Counter2 /> */}
-
       {/* <Toggle /> */}
-
       {/* <InputExample /> */}
-
       {/* <Profile /> */}
-
       {/* <User /> */}
       {/* <Fruits /> */}
       {/* <ArrayState /> */}
       {/* <EmployeeState /> */}
-      <UserGreeting />
+
+      {/* <UserGreeting /> */}/
+      
     </>
   );
 }
+
 export default App;
